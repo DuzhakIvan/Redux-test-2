ff9f5124c5a701e49a2a68963cc66f8b2caecfde
IvanDuzhak <ivanduzhak@gmail.com>
2023-07-10 14:32:29 +0300

    Redux toolkit configureStore()

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
-import { createStore, // Импортируем функцию по созданию Redux store,
-         combineReducers, // функцию combine для обьединения reducers
-         compose, // функцию compose для обьединения усилителей enhancer
-         applyMiddleware } // функция для обьединения middleware
-        from 'redux';  
-import ReduxThunk from 'redux-thunk' // импорнируем библиотеку middleware для возможности передачи функции в качестве action в reducer
+// import { createStore, // Импортируем функцию по созданию Redux store,
+//          combineReducers, // функцию combine для обьединения reducers
+//          compose, // функцию compose для обьединения усилителей enhancer
+//          applyMiddleware } // функция для обьединения middleware
+        // from 'redux';  
+// Теперь эти команды не используются благодаря Toolkit
+import { configureStore } from '@reduxjs/toolkit';
+// import ReduxThunk from 'redux-thunk' // импорнируем библиотеку middleware для возможности передачи функции в качестве action в reducer
 import heroes from '../reducers/heroes'
 import filters from '../reducers/filters';
 
@@ -19,14 +21,22 @@ const stringMiddleware = (store) => // в store у нас доступно {disp
     return next(action); // если не строка, то просто dispath action как по старому
 }
 
-const store = createStore( // Создаем store должны передать reducers
-    combineReducers({heroes, filters}), //  в функцию combineReducer передается обьект с reducers, запись {heroes, filters} = {heroes: heroes, filters: filters}
+// const store = createStore( // Создаем store должны передать reducers
+//     combineReducers({heroes, filters}), //  в функцию combineReducer передается обьект с reducers, запись {heroes, filters} = {heroes: heroes, filters: filters}
    
-    compose( // в функцию compose пиередаем все функции усилители, но необходимо соблюдать правильный порядок
-        applyMiddleware(ReduxThunk, stringMiddleware), // педедаем Middleware но оборачиваем ее в функцию applyMiddleware
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // команда для работы плагина REDUX для браузера
-    )); // Вторым аргументом в createStore функция она является усилителем store 
+//     compose( // в функцию compose пиередаем все функции усилители, но необходимо соблюдать правильный порядок
+//         applyMiddleware(ReduxThunk, stringMiddleware), // педедаем Middleware но оборачиваем ее в функцию applyMiddleware
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // команда для работы плагина REDUX для браузера
+//     )); // Вторым аргументом в createStore функция она является усилителем store 
+
+
+
+const store = configureStore({ // создаем store с помощью configureStore(), внутрь передаем обьект с нужными параметрами
+    reducer: {heroes, filters}, // уже не нужно использовать combineReducers, а просто записываем обьект
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware), // подключаем viddleware из ToolKit и добавляем в массив наш middleware
+    // preloadedState: // оптциональный параметр для задания состояние начального хранилища
+    devTools: process.env.NODE_ENV !== 'production', // Это переменная окружения в Node.js, если мы не в режиме production то ToolKit будет включено
+    
+})
 
 export default store;
-
-// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // команда для работы плагина REDUX для браузера
